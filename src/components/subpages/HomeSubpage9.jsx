cc2111e96e10d0df7ca64f4f7eeb64ec39244fc0
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-17 00:20:38 +0530

    updated ev cahrging subPage

--- src/components/subpages/HomeSubpage9.jsx
+++ src/components/subpages/HomeSubpage9.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import elipse from '../../assets/elipse.svg'
 import evCharge from '../../assets/evCharge.svg'
+import evMap from '../../assets/evMap.svg'
+
 const HomeSubpage9 = () => {
   return (
-    <div>
+    <div >
          
-        <div className="flex flex-col  items-center text-center">
+        <div className="flex flex-col  items-center text-center mt-5">
             {/* heading */}
             <div className="">
              <h1 className="lg:text-[53px] md:text-[42px] sm:text-[32px] text-[28px] font-semibold bg-gradient-to-r from-blue-900 via-indigo-800 to-yellow-400 inline-block text-transparent bg-clip-text">Charging In All Parts Of The Country</h1>
             </div>
 
-        <div className="flex flex-col md:flex-row w-full  justify-around items-center md:mt-9 mt-[-20px]">
+
+             {/* world map */}
+            <div className="flex flex-col items-center mt-6">
+                <img src={evMap} alt="word-MAp" className='absolute' />
+            </div>
+
+
+        <div className="flex flex-col md:flex-row w-full  justify-around items-center md:mt-9 mt-[-20px] md:space-y-0 space-y-6 ">
             {/* left */}
-            <div className="">
+            <div className="md:mr-0 mr-[95px]">
               
-            <div class="space-y-5 mt-8 md:mx-1 mx-auto px-6  text-left">
-            <h1 class="text-indigo-950 dark:text-indigo-400/50 md:text-[42px] text-3xl font-semibold ">EV Charging</h1>
-            <h2 class="text-yellow-700 font-semibold md:text-2xl text-xl pb-7">In 3 Simple Steps</h2>
+            <div class="md:space-y-5  space-y-2 mt-8 md:mx-1 mx-auto px-6  text-left">
+            <h1 class="text-indigo-950 dark:text-indigo-400/50 md:text-[42px] text-2xl font-semibold ">EV Charging</h1>
+            <h2 class="text-yellow-700 font-semibold md:text-2xl text-xl md:pb-7">In 3 Simple Steps</h2>
             <ul class="space-y-2 md:text-xl  text-slate-800 dark:text-slate-500">
               <li class="flex space-x-3 "><img src={elipse} alt="" class="w-5 mt-1"/><p><span className=' font-bold'>Discover</span> Charging Stations</p></li>
               <li class="flex space-x-3 "><img src={elipse} alt="" class="w-5 mt-1"/><p> <span className=' font-bold'>Charge</span> seamlessly</p></li>
