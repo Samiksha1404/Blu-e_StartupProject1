a73511ce964a4f95b4f084c6c3ad06640915d9be
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-17 23:00:31 +0530

    updated ev charging solution page

--- src/components/subpages/cahrgingSolution/ChargingSolution.jsx
+++ src/components/subpages/cahrgingSolution/ChargingSolution.jsx
@@ -22,10 +22,10 @@ const ChargingSolution = () => {
             <div className="text-center space-y-4 lg:mx-0 mx-5">
               <p className="lg:text-[59px] md:text-[42px] text-[32px] font-bold  bg-clip-text text-transparent bg-gradient-to-r from-blue-900 via-blue-950 to-yellow-400  text-center ">EV CHARGING SOLUTIONS FOR YOU</p>
               <p className="lg:text-[25px] text-[20px] dark:text-slate-400 ">We provide EV charging solution all the existing and upcoming infrastructure.</p>
-              <p className="text-slate-500 ">Partner with us to make your establishment EV ready and further boost your profits.</p>
+              <p className="text-slate-500 text-lg ">Partner with us to make your establishment EV ready and further boost your profits.</p>
             </div>
 
-            <div className="mt-5 grid lg:grid-cols-5 gap-3 md:grid-cols-3 grid-cols-2 ">
+            <div className="mt-5 grid lg:grid-cols-5 gap-3 md:grid-cols-3 grid-cols-3  ">
               <Cards bgimage={bghome} icon={home} heading="HOME"/>
               <Cards bgimage={bgfleet} icon={fleet} heading="FLEET"/>
               <Cards bgimage={bgcommercial} icon={commercial} heading="COMMERCIAL"/>
